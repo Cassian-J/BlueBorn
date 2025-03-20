48f36b3eeaefeb1969c9db1e2bf392c8a4e1dec1
Cy4n1 <156065290+Cassian-J@users.noreply.github.com>
2025-03-20 16:40:29 +0100

    modif joystick

--- Assets/Scripts/playerJoystic.cs
+++ Assets/Scripts/playerJoystic.cs
@@ -4,29 +4,8 @@ using UnityEngine;
 
 public class PlayerJoystic : Player
 {
-    [Header("Player Settings")]
-    public int playerID = 1; // 1 ou 2 pour différencier les joueurs
-    public GameObject _hammer;
-    public GameObject _bulletPrefab;
-    public Animator animator;
-    public Transform firePoint;
-
-    [Header("Stats")]
-    public float health = 100;
-    public float attack = 10;
-    public float speed = 2;
-    public float rotationSpeed = 100;
-    public float fireRate = 5;
-    public float projectileSpeed = 10;
-
-    private float nextFireTime = 0f;
-
-    private void Update()
-    {
-        HandleJoystickInput();
-    }
-
-    private void HandleJoystickInput()
+    
+    private void HandleAction()
     {
         float moveX = Input.GetAxis($"Horizontal");
         float moveY = Input.GetAxis($"Vertical");
@@ -41,6 +20,7 @@ public class PlayerJoystic : Player
         if (Input.GetButtonDown($"isShooting"))
         {
             Shoot(moveDirection);
+            nextFireTime = Time.time + 1 / fireRate;
         }
 
         if (Input.GetButtonDown($"isAttacking"))
@@ -49,32 +29,4 @@ public class PlayerJoystic : Player
         }
     }
 
-    private void Shoot(Vector3 vector)
-    {
-        animator.SetBool("isShooting", true);
-        GameObject bullet = Instantiate(_bulletPrefab, firePoint.position, Quaternion.identity);
-        Bullet bulletScript = bullet.GetComponent<Bullet>();
-        if (bulletScript != null)
-        {
-            bulletScript.playerID = playerID;
-            bulletScript.speed = projectileSpeed;
-            bulletScript.move = vector;
-        }
-    }
-
-    private void MeleeAttack()
-    {
-        animator.SetBool("isAttacking", true);
-    }
-
-    public void TakeDamage(float damage)
-    {
-        health -= damage;
-        if (health <= 0) Die();
-    }
-
-    private void Die()
-    {
-        Destroy(gameObject);
-    }
 }
